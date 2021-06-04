4695bd72e68f226514d2a1134de0f52abf00eaa7
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-03 20:51:25 -0400

    Initial commit

---
+++ src/Exercise3.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+import java.util.Scanner;
+public class Exercise3 {
+    public static void main(String[] args){
+        System.out.print("What is the quote? ");
+        Scanner scan = new Scanner(System.in);
+        String quote = scan.nextLine();
+        System.out.print("Who said it? ");
+        String author = scan.nextLine();
+        System.out.print(author + " says, \"" + quote + ".\"");
+    }
+}
